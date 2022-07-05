de15c09198300f97e19d3bb038d4295abcec3cda
Malik Elbadri <elbadrimalik@gmail.com>
2022-07-05 10:25:56 +0300

    gitignore updated

---
+++ src/components/Refused.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="refused">
+    <h1>Camera Permission Is Rquired</h1>
+    <p>Enable Camera Permission And Reload The Page</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.refused {
+  background-color: rgba(65, 0, 51, 0.637);
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
